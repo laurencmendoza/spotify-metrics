efbbd9fe4588c44e4dc9d9d61f36e00746e3a065
laurencmendoza <lauren.camille.mendoza@gmail.com>
2023-09-08 12:15:22 -0700

    edit max width for home page content

--- frontend/src/components/Nav/Nav.js
+++ frontend/src/components/Nav/Nav.js
@@ -6,13 +6,13 @@ export default function Nav() {
         <div className="p-[1.5rem] bg-[black] h-full text-[white] text-[1.5rem]">
         <nav className="flex flex-col justify-start">
             <img className="h-[70px] w-[70px] mx-auto my-0" src={spotifyLogo} alt="spotify logo in black"/>
-            <Link className="mt-12 mb-8 pl-6 font-bold" to="/">
+            <Link className="mt-[100px] mb-8 pl-4 font-bold" to="/">
                 Home
             </Link>
-            <Link className="my-8 pl-6 font-bold" to="/playlists">
+            <Link className="my-8 px-4 font-bold" to="/playlists">
                 Playlists
             </Link>
-            <Link className="my-8 pl-6 font-bold" to="/metrics/medium_term">
+            <Link className="my-8 px-4 font-bold" to="/metrics/medium_term">
                 Metrics
             </Link>
         </nav>
