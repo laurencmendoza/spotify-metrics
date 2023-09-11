ed01ca8cf225545b7aee0f8ba0963c0310a59a60
laurencmendoza <lauren.camille.mendoza@gmail.com>
2023-09-11 11:41:40 -0700

    fix flex toggle for small screens and larger on playlist details page

--- frontend/src/pages/Playlists/PlaylistDetails/index.jsx
+++ frontend/src/pages/Playlists/PlaylistDetails/index.jsx
@@ -38,11 +38,11 @@ export default function PlaylistDetails() {
     return (
         <div className="2xl:flex 2xl:justify-center">
             {playlist ? (
-                <div className="my-10 sm:m-10 flex justify-center">
+                <div className="my-10 sm:m-10 sm:flex justify-center">
                     {playlist.images[0] ? (<img className="h-[240px] mx-auto sm:mx-0" src={playlist.images[0].url} alt={playlist.name}/>) 
                     : (<img className="h-[240px] mx-auto sm:mx-0 bg-[white]" src="https://cdnweb.anghami.com/web/assets/img/placeholders/playlist-placeholder.png"/>)}
                     <div className="m-10">
-                        <h1 className="text-center mx-10 mb-10 text-[1.5rem] sm:text-[1.75rem] font-bold">{playlist.name}</h1>
+                        <h1 className="text-center mb-10 text-[1.5rem] sm:text-[1.75rem] font-bold">{playlist.name}</h1>
                         <a href={playlist.external_urls.spotify} target="_blank">
                             <button className="block mx-auto text-[white] font-bold bg-spotify-green rounded-3xl h-[3rem] py-0 px-[24px] divide-none hover:scale-[1.02]">
                                 LISTEN ON SPOTIFY
